35edff466bde1c16548b787c15f50891e58fe98a
Juhyeong Ko <dury.ko@gmail.com>
2023-02-20 05:03:09 +0900

    feat: dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -157,6 +157,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
